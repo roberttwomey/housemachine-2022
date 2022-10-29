a7dcd360528d54a6104004d8b205ac5e1bf2f276
Robert Twomey <robert.twomey@gmail.com>
2022-10-29 08:51:42 -0700

    moved tests to sandbox

--- extract_stills.py
+++ extract_stills.py
@@ -1,15 +1,111 @@
 import ffmpeg
+import os
 
-# filename ="video.mov"
-filename ="input-videos/livingroom/livingroom_motion_2017-08-13_10.26.55_5.mp4"
+def load_metadata(filename, prepostroll=10.0):
+    metadata = ffmpeg.probe(filename)
+    video_stream = next((stream for stream in metadata['streams'] if stream['codec_type'] == 'video'), None)
 
-metadata = ffmpeg.probe(filename)
-video_stream = next((stream for stream in metadata['streams'] if stream['codec_type'] == 'video'), None)
+    fps = float(metadata['streams'][0]['avg_frame_rate'].split('/')[0])/1000.0
+    duration = float(metadata['streams'][0]['duration'])
 
-fps = metadata['streams'][0]['avg_frame_rate'].split('/')[0]
-duration = metadata['streams'][0]['duration']
+    width = int(video_stream['width'])
+    height = int(video_stream['height'])
 
-width = int(video_stream['width'])
-height = int(video_stream['height'])
+    print(f'Width: {width}, Height: {height}, FPS: {fps} fps, Duration: {duration} sec')
+    print(fps)
 
-print(f'Width: {width}, Height: {height}, FPS: {fps}, Duration: {duration}')
+    # calculating frame intervals
+    # from here: https://api.video/blog/tutorials/extract-a-set-of-frames-from-a-video-with-ffmpeg-and-python
+    action_duration = duration-prepostroll # prepostroll is length of pre and post in motion trigger
+    print(action_duration)
+    
+    return (duration, action_duration, width, height)
+
+def create_intervals(filename, duration, action_duration):
+    # extract before (pre-motion)
+    intervals = [2.5]
+
+    # sample motion section (1fps or similar)
+    # if action_duration >= 1.0:
+    #     parts = 3
+    # else: 
+    #     parts = 1
+
+    parts = int(action_duration/10.0)+1
+    middlepoints = [5.0+((i+1)*action_duration)/(parts+1) for i in range(parts)]
+    intervals += middlepoints
+
+    # extract after (post_motion)
+    intervals+=[duration-2.5]
+
+    print(intervals)
+    return intervals
+
+def extract_frames(filename, intervals, height, outpath):
+    
+    path, file = os.path.split(filename)
+    workfolder = path.split("/")[-1]
+    filebase = os.path.splitext(file)[0]
+
+    i=0
+    for time in intervals:
+        outfilename = "{0}_{1:02d}.jpg".format(filebase, i)
+        outfile = os.path.join(outpath, outfilename)
+        print(i, intervals, filename, outfile)
+
+        (
+            ffmpeg
+            .input(filename, ss=time)
+            .filter('scale', height, height) # width, -1)
+            .output(outfile, vframes=1)
+            .overwrite_output()
+            .run()
+        )
+        i += 1
+
+def walk_files(path):
+    data_dirs = [ 
+        "basement", 
+        "childsroom", 
+        "diningroom", 
+        "frontporch",
+        "kitchen",
+        "livingroom",
+        "studio"
+        ] 
+
+    vidfiles = []
+    counts = {}
+    for dirpath, subdirs, files in os.walk(path):
+        for x in files:
+            if x.endswith(".mp4"):
+                lastsubfolder = dirpath.split("/")[-1]
+                if lastsubfolder in data_dirs:
+                    if lastsubfolder in counts:
+                        counts[lastsubfolder]+=1
+                    else:
+                        counts[lastsubfolder]=1
+                    vidfiles.append(os.path.join(dirpath, x))
+
+    return (vidfiles, counts)
+
+def main():
+    
+    # filename ="video.mov"
+    # filename ="input-videos/livingroom/livingroom_motion_2017-08-13_10.26.55_5.mp4"
+
+    files, counts = walk_files("input-videos/")
+    print(files[:10])
+    print(counts)
+
+    for filename in files:
+        duration, action_duration, width, height = load_metadata(filename)
+        
+        intervals = create_intervals(filename, duration, action_duration)
+
+        extract_frames(filename, intervals, height, outpath="output/")
+        # print(filename, duration)
+
+
+if __name__ == "__main__":
+    main()
--- extract_stills.sh
+++ extract_stills.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
-dirs=(basement childsroom diningroom frontporch gallery kitchen livingroom studio)
-
-for file in input-videos/*/*.avi; do
+#dirs=(basement childsroom diningroom frontporch gallery kitchen livingroom studio)
+for file in input-videos/*/*.mp4; do
     destination="output${file:12:${#file}-17}";
-    mkdir -p "$destination";
-    ffmpeg -i "$file" -r 1 "$destination/image-%d.jpeg";
+    # echo "$destination | cut -d'/' -f 3";
+    filename=$(echo $destination | cut -d'/' -f 3);
+    # echo $filename
+    # duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$file")
+    # remainder=$(echo "$duration"-10 | bc)
+    # echo $remainder
+    
+    ffmpeg -ss 0:10 -hide_banner -loglevel error -i "$file" -q:v 1 -r 3 "output/$filename-%d.jpg";
+    # ffmpeg -ss 0:10 -r 1 -hide_banner -loglevel error -i "$file" -frames:v 1 -q:v 1 "output/$filename-%d.jpg";
+    # ffmpeg -ss 0:11 -hide_banner -loglevel error -i "$file" -frames:v 1 -q:v 1 "$destination-%d.jpg";
+
+    # ORIGINAL with nested subdirectories
+    # mkdir -p "$destination";
+    # ffmpeg -skip_frame nokey -i "$file"-vsync 0 -r 3 "$destination/image-%d.jpg";
+
 done
+
+# #!/bin/bash
+# #dirs=(basement childsroom diningroom frontporch gallery kitchen livingroom studio)
+# for file in input-videos/*/*.mp4; do
+#     destination="output${file:12:${#file}-17}";
+#     mkdir -p "$destination";
+#     ffmpeg -i "$file" -r 1 "$destination/image-%d.jpg";
+# done
+
+# getting time from: https://superuser.com/a/945604
+# ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$file" -sexagesimal
